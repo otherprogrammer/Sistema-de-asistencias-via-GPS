cef2609336cc7a0a967365530b6c8a8b2eaa718d
GDLM1208 <gllerena1208@gmail.com>
2025-10-01 20:38:49 -0500

    Rutas para la app

--- web-app/src/router.tsx
+++ web-app/src/router.tsx
@@ -1,4 +1,3 @@
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProtectedRoute from './components/ProtectedRoute';
 import PublicRoute from './components/PublicRoute';
@@ -8,39 +7,40 @@ import LoginPage from './pages/LoginPage';
 import NotFoundPage from './pages/NotFoundPage';
 import UsersPage from './pages/UsersPage';
 import AdminWorkers from './pages/AdminWorker';
+import WorksitesPage from './pages/WorksitesPage';
 
 export const AppRouter = () => {
-    return (
-        <Router>
-        <Routes>
-          {/* Rutas públicas - Solo accesibles si NO está autenticado */}
-          <Route
-            path="/login"
-            element={
-              <PublicRoute>
-                <LoginPage />
-              </PublicRoute>
-            }
-          />
+  return (
+    <Router>
+      <Routes>
+        {/* Rutas públicas - Solo accesibles si NO está autenticado */}
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <LoginPage />
+            </PublicRoute>
+          }
+        />
 
-          {/* Rutas protegidas - Solo accesibles si está autenticado como admin */}
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <MainLayout />
-              </ProtectedRoute>
-            }
-          >
-            {/* Rutas anidadas dentro del layout */}
-            <Route index element={<AdminWorkers />} />
-            <Route path="gestion-trabajadores" element={<AdminWorkers />} />
-            <Route path="users" element={<UsersPage />} />
-          </Route>
+        {/* Rutas protegidas - Solo accesibles si está autenticado como admin */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <MainLayout />
+            </ProtectedRoute>
+          }
+        >
+          {/* Rutas anidadas dentro del layout */}
+          <Route index element={<AdminWorkers />} />
+          <Route path="gestion-trabajadores" element={<AdminWorkers />} />
+          <Route path="gestion-obras" element={<WorksitesPage />} />
+        </Route>
 
-          {/* Ruta 404 */}
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Router>
-    )
+        {/* Ruta 404 */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
+  );
 };
